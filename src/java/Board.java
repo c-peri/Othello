9f1b49c36c6702bc365295802dd3e17169bd0261
c-peri <christinaperifana@gmail.com>
2025-11-13 18:20:32 +0200

    Adding stuff

--- src/java/Board.java
+++ src/java/Board.java
@@ -21,16 +21,48 @@ class Board
         for(int i = 0; i < this.gameBoard.length; i++)
         {
             for(int j = 0; j < this.gameBoard.length; j++)
-            {
+            if(i == 3) {
+                this.gameBoard[i][3] = W;
+                this.gameBoard[i][4] = B;
+            } else if(i == 4) {
+                this.gameBoard[i][3] = B;
+                this.gameBoard[i][4] = W;
+            } else {
                 this.gameBoard[i][j] = EMPTY;
             }
         }
     }
 
     // copy constructor
-    public Board(Board board) {}
+    public Board(Board board) {
+        this.lastMove = board.lastMove;
+        this.lastPlayer = board.lastPlayer;
+        this.gameBoard = new int[8][8];
+        for(int i = 0; i < this.gameBoard.length; i++) {
+            for(int j = 0; j < this.gameBoard.length; j++)
+            {
+                this.gameBoard[i][j] = board.gameBoard[i][j];
+            }
+        }
+    }
 
-    public void print() {}
+    public void print() {
+        System.out.println("  A B C D E F G H");
+        for(int row = 0; row < 8; row++) {
+            System.out.print((row+1)" |");
+            for(int col = 0; col < 8; col++) {
+                switch (this.gameBoard[row][col]) {
+                    case W -> System.out.print("⦿ ");
+                    case B -> System.out.print("⦾ ");
+                    case EMPTY -> System.out.print("- ");
+                    default -> {
+                    }
+                }
+                System.out.println("|");
+            }
+            System.out.println(" ----------------");
+        }
+    }
 
     ArrayList<Board> getChildren(int letter) {return null;}
 
--- src/java/Main.java
+++ src/java/Main.java
@@ -1,3 +1,14 @@
-public static void main(String[] args) {
 
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //We create the players and the board
+        //MaxDepth for the MiniMax algorithm is set to 2; feel free to change the values
+        Player playerW = new Player(2, Board.W);
+        Player playerB = new Player(2, Board.B);
+        Board board = new Board();
+        board.print();
+
+    }
 }
