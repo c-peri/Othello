2cc6f1f375a7aa294c4d393eef1aba219cb42616
AlexMariaM <alexmaria.mazi@gmail.com>
2025-11-14 23:28:17 +0200

    Made the first move as the black player

--- src/java/Board.java
+++ src/java/Board.java
@@ -2,7 +2,10 @@ import java.util.ArrayList;
 
 class Board {
 
-    //For the black and white circles of the board to show in cmd we need to run "chcp 65001" first.
+    /*
+        For the black and white circles of the board,as well as the lines,
+        to show in cmd we need to run "chcp 65001" first.
+    */
 
     public static final int W = 1;      //If score > 0 => white is ahead
     public static final int B = -1;     //If score < 0 => black is ahead
@@ -24,19 +27,21 @@ class Board {
     public Board() {
 
         this.lastMove = new Move();
-        this.lastPlayer = 0;
+        this.lastPlayer = 1;
         this.gameBoard = new int[dimension][dimension];
 
         for(int i = 0; i < this.gameBoard.length; i++) {
             for(int j = 0; j < this.gameBoard.length; j++) {
-                if (i == 3) {
-                    this.gameBoard[i][3] = W;
-                    this.gameBoard[i][4] = B;
-                } else if (i == 4) {
-                    this.gameBoard[i][3] = B;
-                    this.gameBoard[i][4] = W;
-                } else {
-                    this.gameBoard[i][j] = EMPTY;
+                switch (i) {
+                    case 3 -> {
+                        this.gameBoard[i][3] = W;
+                        this.gameBoard[i][4] = B;
+                    }
+                    case 4 -> {
+                        this.gameBoard[i][3] = B;
+                        this.gameBoard[i][4] = W;
+                    }
+                    default -> this.gameBoard[i][j] = EMPTY;
                 }
             }
         }
@@ -51,9 +56,7 @@ class Board {
         this.gameBoard = new int[dimension][dimension];
 
         for(int i = 0; i < this.gameBoard.length; i++) {
-            for(int j = 0; j < this.gameBoard.length; j++) {
-                this.gameBoard[i][j] = board.gameBoard[i][j];
-            }
+            System.arraycopy(board.gameBoard[i], 0, this.gameBoard[i], 0, this.gameBoard.length);
         }
 
     }
@@ -66,11 +69,12 @@ class Board {
     */
     public void print() {
 
-        System.out.println("\n    A B C D E F G H" +
-                           "\n  ┌─────────────────┐");
+        System.out.println("──────────────────────────────────────────────────────────"+
+                           "\n                       A B C D E F G H" +
+                           "\n                     ┌─────────────────┐");
 
         for(int row = 0; row < this.dimension; row++) {
-            System.out.print((row+1)+" │ ");
+            System.out.print("                   "+(row+1)+" │ ");
             for(int col = 0; col < this.dimension; col++) {
                 switch (this.gameBoard[row][col]) {
                     case W -> System.out.print("● ");
@@ -83,30 +87,32 @@ class Board {
             System.out.println("│");
         }
 
-        System.out.println("  └─────────────────┘");
+        System.out.println("                     └─────────────────┘"+
+                           "\n──────────────────────────────────────────────────────────");
 
     }
 
     //Make a move; it places a letter in the board
-    public void makeMove(int row, int col, int letter) {
+    void makeMove(int row, int col, int letter) {
         this.gameBoard[row][col] = letter;
         this.lastMove = new Move(row, col);
         this.lastPlayer = letter;
     }
 
     /*
-        Method to check whether a move is valid.If:
+        Method to check whether a move is valid. If:
         -The square isn't empty => invalid
         -The square doesn't meet the criteria of Othello => invalid
         -The square is out of bounds => invalid
     */
     public boolean isValidMove(int row, int col) {
+
         if((row > 7) || (col > 7) || (row < 0) || (col < 0)) return false;
         if(this.gameBoard[row][col] != EMPTY) return false;
 
-        //Check horizontaly for a valid move
-        if(col + 2 <= 7){ //Checks for a left valid move
-          if (this.gameBoard[row][col+1] == lastPlayer && (this.gameBoard[row][col+2] != lastPlayer && this.gameBoard[row][col+2] != EMPTY)){
+        //Check horizontally for a valid move
+        if (col + 2 <= 7){ //Checks for a left valid move
+            if (this.gameBoard[row][col+1] == lastPlayer && (this.gameBoard[row][col+2] != lastPlayer && this.gameBoard[row][col+2] != EMPTY)){
               return true;
           }
         }
@@ -149,14 +155,26 @@ class Board {
                 return true;
             }
         }
+
         return false;
+
     }
 
     ArrayList<Board> getChildren(int letter) {return null;}
 
     public int evaluate () {return 0;}
 
-    public boolean isTerminal() {return true;}
+    public boolean isTerminal() {
+        //The board is considered terminal if there are no more empty spaces to place discs in
+        for (int row = 0; row <= 7; row++){
+            for (int col = 0; col <= 7; col++){
+                if (this.gameBoard[row][col] == EMPTY){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
     public Move getLastMove()
     {
@@ -191,6 +209,5 @@ class Board {
     {
         this.lastPlayer = lastPlayer;
     }
-
 
 }
--- src/java/Main.java
+++ src/java/Main.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
 
         //We create the players and the first board
         //MaxDepth for the MiniMax algorithm is initialized at 4 and will change as the game progresses
@@ -9,6 +12,69 @@ public class Main {
         Player playerB = new Player(4, Board.B);
         Board board = new Board();
         board.print();
+
+        int forfeit_counter = 0;
+        while (!board.isTerminal() && forfeit_counter < 2){
+
+            //To ckeck if the board has any legal moves, otherwise the player forfeits their turn
+            boolean forfeit = true;
+            for (int row = 0; row <= 7; row++){
+                for (int col = 0; col <= 7; col++){
+                    if (board.isValidMove(row,col)){
+                        forfeit = false;
+                        break;
+                    }
+                }
+                if (!forfeit){
+                    break;
+                }
+            }
+            //If both of the players need to forfeit their turn the game ends, since the board is considered terminal
+            if (forfeit){
+                forfeit_counter++;
+            } else {
+                forfeit_counter = 0;
+            }
+
+            switch (board.getLastPlayer()){
+                case Board.W -> {
+                    System.out.println("────────────────Black player make your move───────────────");
+                    System.out.print("Insert a row: ");
+                    String row = in.nextLine();
+                    while (!row.matches("[1-8]")) {
+                        System.out.print("Invalid row! Please enter a number from 1 to 8: ");
+                        row = in.nextLine();
+                    }
+                    int row_int = Integer.parseInt(row)-1;
+                    System.out.print("Insert a column: ");
+                    String col = in.nextLine();
+                    while (!col.matches("[A-H]")) {
+                        System.out.print("Invalid column! Please enter a letter from A to H: ");
+                        col = in.nextLine();
+                    }
+                    int col_int = col.charAt(0) - 'A';
+
+                    //Making the move depending on if it's valid or not
+                    if (board.isValidMove(row_int,col_int)){
+                        board.makeMove(row_int,col_int,-1);
+                        board.setLastMove(new Move(row_int,col_int));
+                        board.setLastPlayer(-1);
+                        System.out.println("──────────────────────────────────────────────────────────");
+                        board.print();
+                    } else {
+                        System.out.println("Invalid move! Please make a new move");
+                    }
+
+                    //case Board.B:
+                    //System.out.println("────────────────White player make your move───────────────");
+                }
+                //case Board.B:
+                    //System.out.println("────────────────White player make your move───────────────");
+
+            }
+            //If White played last, then Black plays now
+            
+        }
 
     }
 
